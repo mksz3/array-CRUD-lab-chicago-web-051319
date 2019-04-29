27b28777781a8d56b5d5737e4d2c2c383c4f84a4
Markus Sanders <markus.sanderst@gmail.com>
2019-04-29 18:44:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,16 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  arr = 1..4
+  return arr
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
